4e455c6eae48dc6d03e92417503089a623a14c2b
KFalcao <karollinefalcao@gmail.com>
2025-03-11 20:24:45 -0300

    :sparkle: feat: paginas login e recuperar senha

---
+++ src/pages/login.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+  return (
+    <main className="bg-gradient-to-l from-color-dark-green via-color-white to-color-pink">
+      <img src="" alt="" />
+      <div>
+        <h2>Eco Tye</h2>
+        <p>Gerenciador de Funcionários</p>
+        <form action="" method="get">
+          <input type="text" />
+          <input type="text" />
+          <button type="submit">Entrar</button>
+        </form>
+        <a href="./recovery">Esqueceu a Senha?</a>
+      </div>
+    </main>
+  );
+}
