de111a903e6630e417e9968cab0fe39a1934885d
KFalcao <karollinefalcao@gmail.com>
2025-03-14 11:51:07 -0300

    :sparkles: feat: Lógica formulário login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,18 @@
-export default function Home() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/login";
+import Admin from "../pages/admin";
+
+function App() {
   return (
-    <main>
-      <div className="w-60 h-24 pt-8 bg-color-light-green">
-        <a href="/login">Pagina de login</a>
-      </div>
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/admin" element={<Admin/>} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
+
+
